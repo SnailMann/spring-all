4488b0471e5dd74822df269f2b8aa5b34fd2f621
liwenjie <wenjie.li@shopee.com>
2022-04-26 15:42:44 +0800

    framework init

---
+++ spring-orm/spring-mybatis-plus/src/main/java/com/snailmann/me/mp/SpringMybatisPlusApplication.java
@@ -0,0 +1,16 @@
+package com.snailmann.me.mp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wenjie.li
+ */
+@SpringBootApplication
+public class SpringMybatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMybatisPlusApplication.class, args);
+    }
+
+}
