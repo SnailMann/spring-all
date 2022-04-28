f01d79a7d725ef3ee8769aa2ae3af81be4d02960
liwenjie <wenjie.li@shopee.com>
2022-04-28 10:49:47 +0800

    spring-ws

---
+++ spring-websocket/spring-websocket-demo/src/main/java/com/snailmann/me/spring/ws/SpringWebsocketDemoApplication.java
@@ -0,0 +1,16 @@
+package com.snailmann.me.spring.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wenjie.li
+ */
+@SpringBootApplication
+public class SpringWebsocketDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebsocketDemoApplication.class, args);
+    }
+
+}
