e665d89edad08dd7f99064303781c27de4f4985b
Wenjie Li <wenjie.li@shopee.com>
2022-06-16 16:37:00 +0800

    flink

---
+++ spring-flink/src/main/java/com/snailmann/me/spring/flink/SpringFlinkApplication.java
@@ -0,0 +1,13 @@
+package com.snailmann.me.spring.flink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFlinkApplication.class, args);
+	}
+
+}
---
+++ spring-flink/src/test/java/com/snailmann/me/spring/flink/SpringFlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snailmann.me.spring.flink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
